e8b249d438cc085c7f2d6265c9c54ff354283363
SiriusZhangyu <43713383+SiriusZhangyu@users.noreply.github.com>
2019-05-19 00:30:56 -0400

    Update U-Net.py

--- U-Net/U-Net.py
+++ U-Net/U-Net.py
@@ -1,1 +1,204 @@
+import os
+import numpy as np
+import pandas as pd 
+from skimage.io import imread
+import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
+from skimage.segmentation import mark_boundaries
+from skimage.morphology import binary_opening, disk, label
+import gc; gc.enable()
+import tensorflow as tf
+from skimage.util import montage
+tran_image_dir = os.path.join('./train_v2')
+test_image_dir = os.path.join('./test_v2')
+def rle_decode(mask_rle, shape=(768, 768)):
+   
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape).T  # Needed to align to RLE direction
+  def masks_as_image(in_mask_list):
+    
+    all_masks = np.zeros((768, 768), dtype = np.uint8)
+    for mask in in_mask_list:
+        if isinstance(mask, str):
+            all_masks |= rle_decode(mask)
+    return all_masks
+  masks = pd.read_csv(os.path.join('./train_ship_segmentations_v2.csv'))
+  masks['ships'] = masks['EncodedPixels'].map(lambda c_row: 1 if isinstance(c_row, str) else 0)
+unique_img_ids = masks.groupby('ImageId').agg({'ships': 'sum'}).reset_index()
+unique_img_ids['has_ship'] = unique_img_ids['ships'].map(lambda x: 1.0 if x>0 else 0.0)
+unique_img_ids['has_ship_vec'] = unique_img_ids['has_ship'].map(lambda x: [x])
+unique_img_ids['file_size_kb'] = unique_img_ids['ImageId'].map(lambda x: 
+                                                               os.stat(os.path.join(train_image_dir, 
+                                                                                    x)).st_size/1024)
+unique_img_ids = unique_img_ids[unique_img_ids['file_size_kb'] > 50] # keep only +50kb files
+SAMPLES_PER_GROUP = 1000
+balanced_train_df = unique_img_ids.groupby('ships').apply(lambda x: x.sample(SAMPLES_PER_GROUP) if len(x) > SAMPLES_PER_GROUP else x)
+balanced_train_df= balanced_train_df.sample(min(1000, balanced_train_df.shape[0]))
+from sklearn.model_selection import train_test_split
+train_ids, valid_ids = train_test_split(balanced_train_df, 
+                 test_size = 0.2, 
+                 stratify = balanced_train_df['ships'])
+train_df = pd.merge(masks, train_ids)
+valid_df = pd.merge(masks, valid_ids)
+def make_image_gen(in_df, batch_size = 48):
+    all_batches = list(in_df.groupby('ImageId'))
+    out_rgb = []
+    out_mask = []
+    while True:
+        np.random.shuffle(all_batches)
+        for c_img_id, c_masks in all_batches:
+            rgb_path = os.path.join(train_image_dir, c_img_id)
+            c_img = imread(rgb_path)
+            c_mask = np.expand_dims(masks_as_image(c_masks['EncodedPixels'].values), -1)
+            if IMG_SCALING is not None:
+                c_img = c_img[::IMG_SCALING[0], ::IMG_SCALING[1]]
+                c_mask = c_mask[::IMG_SCALING[0], ::IMG_SCALING[1]]
+            out_rgb += [c_img]
+            out_mask += [c_mask]
+            if len(out_rgb)>=batch_size:
+                yield np.stack(out_rgb, 0)/255.0, np.stack(out_mask, 0)
+                out_rgb, out_mask=[], []
+IMG_SCALING = (3, 3)
+train_gen = make_image_gen(train_df)
+train_x, train_y = next(train_gen)
+montage_rgb = lambda x: np.stack([montage(x[:, :, :, i]) for i in range(x.shape[3])], -1)
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize = (30, 10))
+batch_rgb = montage_rgb(train_x)
+batch_seg = montage(train_y[:, :, :, 0])
+ax1.imshow(batch_rgb)
+ax1.set_title('Images')
+ax2.imshow(batch_seg)
+ax2.set_title('Segmentations')
+ax3.imshow(mark_boundaries(batch_rgb, 
+                           batch_seg.astype(int)))
+ax3.set_title('Outlined Ships')
+fig.savefig('overview.png')
+VALID_IMG_COUNT = 500
+valid_x, valid_y = next(make_image_gen(valid_df, VALID_IMG_COUNT))
+UPSAMPLE_MODE = 'SIMPLE'
+NET_SCALING = (1, 1)
+GAUSSIAN_NOISE = 0.1
+from keras import models, layers
+# Build U-Net model
+def upsample_conv(filters, kernel_size, strides, padding):
+    return layers.Conv2DTranspose(filters, kernel_size, strides=strides, padding=padding)
+def upsample_simple(filters, kernel_size, strides, padding):
+    return layers.UpSampling2D(strides)
 
+if UPSAMPLE_MODE=='DECONV':
+    upsample=upsample_conv
+else:
+    upsample=upsample_simple
+    
+input_img = layers.Input(t_x.shape[1:], name = 'RGB_Input')
+pp_in_layer = input_img
+
+if NET_SCALING is not None:
+    pp_in_layer = layers.AvgPool2D(NET_SCALING)(pp_in_layer)
+    
+pp_in_layer = layers.GaussianNoise(GAUSSIAN_NOISE)(pp_in_layer)
+pp_in_layer = layers.BatchNormalization()(pp_in_layer)
+
+c1 = layers.Conv2D(8, (3, 3), activation='relu', padding='same') (pp_in_layer)
+c1 = layers.Conv2D(8, (3, 3), activation='relu', padding='same') (c1)
+p1 = layers.MaxPooling2D((2, 2)) (c1)
+
+c2 = layers.Conv2D(16, (3, 3), activation='relu', padding='same') (p1)
+c2 = layers.Conv2D(16, (3, 3), activation='relu', padding='same') (c2)
+p2 = layers.MaxPooling2D((2, 2)) (c2)
+
+c3 = layers.Conv2D(32, (3, 3), activation='relu', padding='same') (p2)
+c3 = layers.Conv2D(32, (3, 3), activation='relu', padding='same') (c3)
+p3 = layers.MaxPooling2D((2, 2)) (c3)
+
+c4 = layers.Conv2D(64, (3, 3), activation='relu', padding='same') (p3)
+c4 = layers.Conv2D(64, (3, 3), activation='relu', padding='same') (c4)
+p4 = layers.MaxPooling2D(pool_size=(2, 2)) (c4)
+
+
+c5 = layers.Conv2D(128, (3, 3), activation='relu', padding='same') (p4)
+c5 = layers.Conv2D(128, (3, 3), activation='relu', padding='same') (c5)
+
+u6 = upsample(64, (2, 2), strides=(2, 2), padding='same') (c5)
+u6 = layers.concatenate([u6, c4])
+c6 = layers.Conv2D(64, (3, 3), activation='relu', padding='same') (u6)
+c6 = layers.Conv2D(64, (3, 3), activation='relu', padding='same') (c6)
+
+u7 = upsample(32, (2, 2), strides=(2, 2), padding='same') (c6)
+u7 = layers.concatenate([u7, c3])
+c7 = layers.Conv2D(32, (3, 3), activation='relu', padding='same') (u7)
+c7 = layers.Conv2D(32, (3, 3), activation='relu', padding='same') (c7)
+
+u8 = upsample(16, (2, 2), strides=(2, 2), padding='same') (c7)
+u8 = layers.concatenate([u8, c2])
+c8 = layers.Conv2D(16, (3, 3), activation='relu', padding='same') (u8)
+c8 = layers.Conv2D(16, (3, 3), activation='relu', padding='same') (c8)
+
+u9 = upsample(8, (2, 2), strides=(2, 2), padding='same') (c8)
+u9 = layers.concatenate([u9, c1], axis=3)
+c9 = layers.Conv2D(8, (3, 3), activation='relu', padding='same') (u9)
+c9 = layers.Conv2D(8, (3, 3), activation='relu', padding='same') (c9)
+
+d = layers.Conv2D(1, (1, 1), activation='sigmoid') (c9)
+# d = layers.Cropping2D((EDGE_CROP, EDGE_CROP))(d)
+# d = layers.ZeroPadding2D((EDGE_CROP, EDGE_CROP))(d)
+if NET_SCALING is not None:
+    d = layers.UpSampling2D(NET_SCALING)(d)
+
+seg_model = models.Model(inputs=[input_img], outputs=[d])
+seg_model.summary()
+import keras.backend as K
+from keras.optimizers import Adam
+from keras.losses import binary_crossentropy
+
+## intersection over union
+def IoU(y_true, y_pred, eps=1e-6):
+    if np.max(y_true) == 0.0:
+        return IoU(1-y_true, 1-y_pred) ## empty image; calc IoU of zeros
+    intersection = K.sum(y_true * y_pred, axis=[1,2,3])
+    union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3]) - intersection
+    return -K.mean( (intersection + eps) / (union + eps), axis=0)
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, EarlyStopping, ReduceLROnPlateau
+weight_path="{}_weights.best.hdf5".format('seg_model')
+
+checkpoint = ModelCheckpoint(weight_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min', save_weights_only=True)
+
+reduceLROnPlat = ReduceLROnPlateau(monitor='val_loss', factor=0.33,
+                                   patience=1, verbose=1, mode='min',
+                                   min_delta=0.0001, cooldown=0, min_lr=1e-8)
+
+early = EarlyStopping(monitor="val_loss", mode="min", verbose=2,
+                      patience=20) # probably needs to be more patient, but kaggle time is limited
+
+callbacks_list = [checkpoint, early, reduceLROnPlat]
+seg_model.compile(optimizer=Adam(1e-3, decay=1e-6), loss=IoU, metrics=['binary_accuracy'])
+step_count = min(9, train_df.shape[0]//48)
+loss_history = [seg_model.fit_generator(train_gen,
+                          steps_per_epoch=step_count,
+                          epochs=10,
+                          validation_data=(valid_x, valid_y),
+                          callbacks=callbacks_list,
+                          workers=1 # the generator is not very thread safe
+                                           )]
+samples = valid_df.groupby('ships').apply(lambda x: x.sample(1))
+fig, m_axs = plt.subplots(samples.shape[0], 4, figsize = (15, samples.shape[0]*4))
+[c_ax.axis('off') for c_ax in m_axs.flatten()]
+
+for (ax1, ax2, ax3, ax4), c_img_name in zip(m_axs, samples.ImageId.values):
+    first_seg, first_img = raw_prediction(c_img_name, train_image_dir)
+    ax1.imshow(first_img)
+    ax1.set_title('Image: ' + c_img_name)
+    ax2.imshow(first_seg[:, :, 0], cmap=get_cmap('jet'))
+    ax2.set_title('Model Prediction')
+    reencoded = masks_as_color(multi_rle_encode(smooth(first_seg)[:, :, 0]))
+    ax3.imshow(reencoded)
+    ax3.set_title('Prediction Masks')
+    ground_truth = masks_as_color(masks.query('ImageId=="{}"'.format(c_img_name))['EncodedPixels'])
+    ax4.imshow(ground_truth)
+    ax4.set_title('Ground Truth')
